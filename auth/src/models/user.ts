7906c8a5f98784f64bba3e97b63aff36e0a37d3d
Wolfgang Becker <5240716+wolfgangbecker@users.noreply.github.com>
2021-09-28 00:18:41 +0200

    Hash user password on change

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Password } from "../services/password";
 
 // Interface to describe the properties needed to create a new User
 interface UserAttrs {
@@ -26,6 +27,15 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+
+  done();
 });
 
 userSchema.statics.build = (attrs: UserAttrs) => {
